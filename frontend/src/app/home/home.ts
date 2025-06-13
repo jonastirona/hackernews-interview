4481bdc4df4ee9638676d6e3c21735dd2d0ec4d8
Jonas Tirona <jonastirona1@gmail.com>
2025-06-13 04:55:51 -0400

    clean up directory

--- frontend/src/app/home/home.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home',
-  standalone: true,
-  template: '<h1>hi</h1>',
-  styles: []
-})
-export class HomeComponent {} 
